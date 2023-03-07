cf51d19f57ba310ccf694e4b4752990e6d93d331
GladysKigen <gladys.kigen@student.moringa.com>
2023-03-07 13:31:06 +0300

    completed

--- lib/student.rb
+++ lib/student.rb
@@ -4,9 +4,9 @@ class Student
   
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-  attr_accessor :name, :grade, id
+  attr_accessor :name, :grade, :id
 #methods
-  def initilialize(id=nil, name, grade)
+  def initialize(id=nil, name, grade)
   @id =id
   @name = name
   @grade = grade
@@ -19,7 +19,6 @@ class Student
       id INTEGER PRIMARY KEY,
       name TEXT,
       grade INTEGER
-
     )
     SQL
     DB[:conn].execute(sql)
@@ -27,7 +26,7 @@ class Student
 
   def self.drop_table
     sql= <<-SQL
-    DROP TABLE IF EXISTS students (
+    DROP TABLE IF EXISTS students 
     SQL
     DB[:conn].execute(sql)
   end
@@ -43,8 +42,8 @@ class Student
       INSERT INTO students (name, grade)
       VALUES (?,?)
       SQL
-      DB[:conn].execute(sql, self.name,self.grade)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0[0]]
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
   end
 
@@ -59,7 +58,6 @@ class Student
     id = row[0]
     name =row[1]
     grade = row[2]
-
     self.new(id, name, grade)
   end
 
@@ -67,10 +65,10 @@ class Student
   #his class method takes in an argument of a name. It queries the database table for a record that has a name of the name passed in as an argument. Then it uses the #new_from_db method to instantiate a Student object with the database row that the SQL query returns.
   def self.find_by_name(name)
     sql = <<-SQL
-    SELECT * FROM students WHERE name =? LIMIT 1
+    SELECT * FROM students WHERE name = ? LIMIT 1
     SQL
 
-    DB [:conn].execute(sql, name).collect {|row| self.new_from_db(row)}.first
+    DB[:conn].execute(sql, name).collect {|row| self.new_from_db(row)}.first
   end
 
   #This method updates the database row mapped to the given Student instance.
